0dad6b2bd1f1e84c42892c8ed0552ea1a30b0984
Daddahu <70315095+sorosk@users.noreply.github.com>
2021-07-08 18:53:30 +0200

    added variables for animation

--- app.js
+++ app.js
@@ -24,7 +24,16 @@ function init(){
     function nextSlide(pageNumber){
         const nextPage = pages[pageNumber];
         const currentPage = pages[current];
+        const nextLeft = nextPage.querySelector(".hero .model-left");
+        const nextRight = nextPage.querySelector(".hero .model-right");
+        const currentLeft = currentPage.querySelector(".hero .model-left");
+        const currentRight = currentPage.querySelector(".hero .model-right");
+        const nextText = nextPage.querySelector(".details");
+        const portfolio = document.querySelector(".portofolio")
 
+        const t1 = new TimelineMax(); //FIXME get webstorm to identify gsap
+        t1.fromTo(currentLeft, 0.3, {y: "-10%"}, {y: "-100%"})
+            .fromTo(currentRight, 0.3, {y: "10%"}, {y: "100%"}, "-=0.2");
     }
 }
 
